90f062e0cbe6b11d6b7ad7139fbc040c97d356d1
Israel Calderón <israel@flat.mx>
2021-04-27 10:50:45 -0500

    initial commit

---
+++ flatonacci.py
@@ -0,0 +1,28 @@
+"""
+Flatonacci secuence is a secuence which is result from the same given secuence 
+plus the sum of the last 3 numbers of the secuence. 
+
+The challenge is to create a flatonacci function that given a signature returns the first 
+n elements - signature included of the so seeded sequence. So, if we are to 
+start our Flatonacci sequence with [1, 1, 1] as a starting input (AKA signature) and n = 8,
+we have this sequence:
+
+[1, 1 ,1, 3, 5, 9, 17, 31]
+
+Another example; if signature is the secuence [0, 0, 1] we should get some thing
+like:
+
+[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+
+- Signature will always contain 3 numbers 
+- n will always be a non-negative number
+- if n == 0, then return an empty list and be ready for anything else which is not
+clearly specified ;)
+
+Note. Please note that we are gonna test the funcion against a lot of different signatures and n's
+"""
+
+
+def flatonacci(signature: list, n: int) -> list:
+    # happy coding
+    pass
