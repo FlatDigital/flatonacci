801e1cb77d1760c32ce9a94202a69b270b08f02b
ldeleond <leonel@sfleet.mx>
2021-05-25 23:12:30 -0500

    adding a list return fix :$

--- flatonacci-solution.py
+++ flatonacci-solution.py
@@ -2,13 +2,15 @@ def flatonacci(signature: list, n: int) -> list:
     if len(signature) == 3 and n > 0:
         pos1, pos2, pos3 = signature[-1], signature[-2], signature[-3]
         count = 0        
-        while count < n:           
+        newlist = []
+        while count < n:   
+            newlist.append(pos1)            
             plus = pos1 + pos2 + pos3            
             pos1 = pos2
             pos2 = pos3
             pos3 = plus
-            count += 1  
-            print(pos1)
+            count += 1              
+        return newlist
     elif  n == 0:
         return list()
     else:
@@ -19,7 +21,7 @@ def flatonacci(signature: list, n: int) -> list:
 #n = int(input("give me a n elements"))
 
 signature = [1,1,1]
-n = 8
+n = 12
 
 #return
 print(flatonacci(signature, n)) 
